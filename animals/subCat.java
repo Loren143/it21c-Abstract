da4072c0ced7056c7b3dc5839a27199102ec6cfc
Loren143 <dacolloren@gmail.com>
2023-11-28 23:31:49 +0800

    added cat class extends

---
+++ animals/subCat.java
@@ -0,0 +1,12 @@
+package animals;
+
+public class Cat extends pets {
+       public Cat(String name){
+           super(name);
+           
+       }
+       public String Color(){
+           return "white";
+           
+       }
+}
